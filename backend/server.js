c09bf62e6cf2bce4041e8d4a81a98105a7e0ff74
mkg101 <manishkumargupta875@gmail.com>
2023-07-08 20:20:42 +0530

    Get products from database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -9,14 +9,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
