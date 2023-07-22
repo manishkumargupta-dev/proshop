b7e4595248f71fa860e43afb2e67321a7af4385a
Manish Kumar Gupta <manishkumargupta875@gmail.com>
2023-07-22 12:32:41 +0530

    error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -14,5 +15,8 @@ app.use("/api/products", productRoutes);
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
