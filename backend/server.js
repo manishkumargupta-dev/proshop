714ea71a8a5406cc7256861cf0a415a5f820aec2
Manish Kumar Gupta <manishkumargupta875@gmail.com>
2023-07-28 16:14:15 +0530

    User routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
@@ -11,6 +12,7 @@ connectDB();
 const app = express();
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
